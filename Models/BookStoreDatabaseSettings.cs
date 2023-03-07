0b178a0376d9a7b2d6767460789e9ea805ef17c2
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-06 21:41:09 -0600

    adding BookstoreBatabaseSettings Class

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiMongoDB.Models
+{
+	public class BookStoreDatabaseSettings
+	{
+		public string ConnectionStrings { get; set; } = null!;
+
+		public string DatabaseName { get; set; } = null!;
+
+		public string BooksCollectionName { get; set; } = null!;
+	}
+}
